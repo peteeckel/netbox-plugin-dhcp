ad9c963364b5de7fcafed48d58a06de7b133b7aa
Peter Eckel <pe-git@hindenburgring.com>
2025-09-26 16:18:07 +0200

    Cleaned up migrations and based them on NetBox 4.0.0

--- netbox_dhcp/migrations/0002_import_option_definitions.py
+++ netbox_dhcp/migrations/0002_import_option_definitions.py
@@ -23,7 +23,7 @@ def load_option_definitions(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("netbox_dhcp", "0030_optiondefinition_description"),
+        ("netbox_dhcp", "0001_initial"),
     ]
 
     operations = [
