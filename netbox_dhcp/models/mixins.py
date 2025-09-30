db7f992604c9487a32af96f403768afb58cb43e5
Peter Eckel <pe-git@hindenburgring.com>
2025-09-30 16:46:31 +0200

    Removed redundant entry from __all__

--- netbox_dhcp/models/mixins.py
+++ netbox_dhcp/models/mixins.py
@@ -17,7 +17,6 @@ __all__ = (
     "ClientClassModelMixin",
     "LifetimeModelMixin",
     "OfferLifetimeModelMixin",
-    "LifetimeModelMixin",
     "DDNSUpdateModelMixin",
     "LeaseModelMixin",
     "NetworkModelMixin",
