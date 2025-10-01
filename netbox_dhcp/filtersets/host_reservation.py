4913fa359592cc3d3526c8f410e48a237b29a15f
Peter Eckel <pe-git@hindenburgring.com>
2025-10-01 13:45:19 +0200

    Implemented the option to filter related objects using text filters (tbc)

--- netbox_dhcp/filtersets/host_reservation.py
+++ netbox_dhcp/filtersets/host_reservation.py
@@ -7,14 +7,15 @@ from dcim.models import MACAddress
 from ipam.models import IPAddress, Prefix
 from ipam.choices import IPAddressFamilyChoices
 
-from netbox_dhcp.models import HostReservation
+from netbox_dhcp.models import HostReservation, DHCPServer, Subnet
 
-from .mixins import ClientClassAssignmentFilterMixin
+from .mixins import BOOTPFilterMixin, ClientClassAssignmentFilterMixin
 
 __all__ = ("HostReservationFilterSet",)
 
 
 class HostReservationFilterSet(
+    BOOTPFilterMixin,
     ClientClassAssignmentFilterMixin,
     NetBoxModelFilterSet,
 ):
@@ -29,11 +30,9 @@ class HostReservationFilterSet(
             "circuit_id",
             "client_id",
             "flex_id",
-            "next_server",
-            "server_hostname",
-            "boot_file_name",
             "comment",
             "hostname",
+            *BOOTPFilterMixin.FILTER_FIELDS,
         )
 
     name = django_filters.CharFilter(
@@ -42,18 +41,38 @@ class HostReservationFilterSet(
     description = django_filters.CharFilter(
         label=_("Description"),
     )
+    duid = django_filters.CharFilter(
+        label=_("DUID"),
+    )
+    circuit_id = django_filters.CharFilter(
+        label=_("Circuit ID"),
+    )
+    client_id = django_filters.CharFilter(
+        label=_("Client ID"),
+    )
+    flex_id = django_filters.CharFilter(
+        label=_("Flex ID"),
+    )
+
     hw_address_id = django_filters.ModelMultipleChoiceFilter(
         queryset=MACAddress.objects.all(),
         field_name="hw_address",
         label=_("Hardware Address ID"),
     )
-
+    hw_address = django_filters.CharFilter(
+        field_name="hw_address__mac_address",
+        label=_("Hardware Address"),
+    )
     ipv4_address_id = django_filters.ModelMultipleChoiceFilter(
         queryset=IPAddress.objects.filter(
             address__family=IPAddressFamilyChoices.FAMILY_4
         ),
         field_name="ipv4_address",
         label=_("IPv4 Address ID"),
+    )
+    ipv4_address = django_filters.CharFilter(
+        field_name="ipv4_address__address",
+        label=_("IPv4 Address"),
     )
     ipv6_address_id = django_filters.ModelMultipleChoiceFilter(
         queryset=IPAddress.objects.filter(
@@ -62,15 +81,51 @@ class HostReservationFilterSet(
         field_name="ipv6_addresses",
         label=_("IPv6 Address ID"),
     )
+    ipv6_address = django_filters.CharFilter(
+        field_name="ipv6_addresses__address",
+        distinct=True,
+        label=_("IPv6 Address"),
+    )
     ipv6_prefix_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Prefix.objects.all(),
+        queryset=Prefix.objects.filter(prefix__family=IPAddressFamilyChoices.FAMILY_6),
         field_name="ipv6_prefixes",
         label=_("IPv6 Prefix ID"),
     )
+    ipv6_prefix = django_filters.CharFilter(
+        field_name="ipv6_prefixes__prefix",
+        distinct=True,
+        label=_("IPv6 Prefix"),
+    )
     excluded_ipv6_prefix_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Prefix.objects.all(),
+        queryset=Prefix.objects.filter(prefix__family=IPAddressFamilyChoices.FAMILY_6),
         field_name="excluded_ipv6_prefixes",
         label=_("Excluded IPv6 Prefix ID"),
+    )
+    excluded_ipv6_prefix = django_filters.CharFilter(
+        field_name="excluded_ipv6_prefixes__prefix",
+        distinct=True,
+        label=_("Excluded IPv6 Prefix"),
+    )
+
+    parent_dhcp_server_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=DHCPServer.objects.all(),
+        field_name="parent_dhcpservers",
+        label=_("Parent DHCP Server ID"),
+    )
+    parent_dhcp_server = django_filters.CharFilter(
+        field_name="parent_dhcpservers__name",
+        distinct=True,
+        label=_("Parent DHCP Server"),
+    )
+    parent_subnet_id = django_filters.ModelMultipleChoiceFilter(
+        queryset=Subnet.objects.all(),
+        field_name="parent_subnets",
+        label=_("Parent Subnet ID"),
+    )
+    parent_subnet = django_filters.CharFilter(
+        field_name="parent_subnets__name",
+        distinct=True,
+        label=_("Parent Subnet"),
     )
 
     def search(self, queryset, name, value):
