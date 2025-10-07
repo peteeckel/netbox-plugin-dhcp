07805b950ad4ad7f55ade809c785cb3ac41f706d
Peter Eckel <pe-git@hindenburgring.com>
2025-10-07 12:47:00 +0200

    Replaced the explicit field list for Subnet with the mixin version

--- netbox_dhcp/filtersets/subnet.py
+++ netbox_dhcp/filtersets/subnet.py
@@ -3,30 +3,41 @@ from django.db.models import Q
 from django.utils.translation import gettext as _
 
 from netbox.filtersets import NetBoxModelFilterSet
-from ipam.models import Prefix
 from ipam.choices import IPAddressFamilyChoices
 
 from netbox_dhcp.models import Subnet
 
 from .mixins import (
+    PrefixFilterMixin,
     ClientClassFilterMixin,
+    BOOTPFilterMixin,
     DDNSUpdateFilterMixin,
+    LifetimeFilterMixin,
+    LeaseFilterMixin,
+    NetworkFilterMixin,
     ChildSubnetFilterMixin,
     ChildPoolFilterMixin,
     ChildPDPoolFilterMixin,
     ChildHostReservationFilterMixin,
+    ParentDHCPServerFilterMixin,
 )
 
 __all__ = ("SubnetFilterSet",)
 
 
 class SubnetFilterSet(
+    PrefixFilterMixin,
     ClientClassFilterMixin,
+    BOOTPFilterMixin,
     DDNSUpdateFilterMixin,
+    LifetimeFilterMixin,
+    LeaseFilterMixin,
+    NetworkFilterMixin,
     ChildSubnetFilterMixin,
     ChildPoolFilterMixin,
     ChildPDPoolFilterMixin,
     ChildHostReservationFilterMixin,
+    ParentDHCPServerFilterMixin,
     NetBoxModelFilterSet,
 ):
     class Meta:
@@ -38,49 +49,11 @@ class SubnetFilterSet(
             "description",
             "family",
             "subnet_id",
-            "next_server",
-            "server_hostname",
-            "boot_file_name",
-            "offer_lifetime",
-            "valid_lifetime",
-            "min_valid_lifetime",
-            "max_valid_lifetime",
-            "preferred_lifetime",
-            "min_preferred_lifetime",
-            "max_preferred_lifetime",
-            "renew_timer",
-            "rebind_timer",
-            "match_client_id",
-            "authoritative",
-            "reservations_global",
-            "reservations_out_of_pool",
-            "reservations_in_subnet",
-            "calculate_tee_times",
-            "t1_percent",
-            "t2_percent",
-            "cache_threshold",
-            "cache_max_age",
-            "adaptive_lease_time_threshold",
-            "store_extended_info",
-            "allocator",
-            "pd_allocator",
-            "relay",
-            "interface_id",
-            "rapid_commit",
-            "hostname_char_set",
-            "hostname_char_replacement",
-            "ddns_send_updates",
-            "ddns_override_no_update",
-            "ddns_override_client_update",
-            "ddns_replace_client_name",
-            "ddns_generated_prefix",
-            "ddns_qualifying_suffix",
-            "ddns_update_on_renew",
-            "ddns_conflict_resolution_mode",
-            "ddns_ttl_percent",
-            "ddns_ttl",
-            "ddns_ttl_min",
-            "ddns_ttl_max",
+            *PrefixFilterMixin.FILTER_FIELDS,
+            *BOOTPFilterMixin.FILTER_FIELDS,
+            *LifetimeFilterMixin.FILTER_FIELDS,
+            *LeaseFilterMixin.FILTER_FIELDS,
+            *DDNSUpdateFilterMixin.FILTER_FIELDS,
             "comment",
         )
 
@@ -95,11 +68,6 @@ class SubnetFilterSet(
         field_name="prefix__prefix",
         lookup_expr="family",
         label=_("Address Family"),
-    )
-    prefix_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Prefix.objects.all(),
-        field_name="prefix",
-        label=_("Prefix"),
     )
 
     def search(self, queryset, name, value):
