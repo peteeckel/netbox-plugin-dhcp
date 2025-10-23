ac9ed55e23e6e19eee780d6a5e57f2235c999231
Peter Eckel <pe-git@hindenburgring.com>
2025-10-23 12:48:27 +0200

    Release 0.0.8

--- netbox_dhcp/__init__.py
+++ netbox_dhcp/__init__.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 from netbox.plugins import PluginConfig
 
-__version__ = "0.0.6"
+__version__ = "0.0.8"
 
 
 class DHCPConfig(PluginConfig):
--- netbox_dhcp/tests/test_netbox_dhcp.py
+++ netbox_dhcp/tests/test_netbox_dhcp.py
@@ -7,7 +7,7 @@ from utilities.testing.api import APITestCase
 
 class NetBoxDHCPVersionTestCase(SimpleTestCase):
     def test_version(self):
-        assert __version__ == "0.0.6"
+        assert __version__ == "0.0.8"
 
 
 class AppTest(APITestCase):
