6a6c73b05960329a38d5d855006c7276f34354cf
Peter Eckel <pe-git@hindenburgring.com>
2026-01-25 17:07:27 +0100

    Prepared release 0.1.3

--- netbox_dhcp/tests/test_netbox_dhcp.py
+++ netbox_dhcp/tests/test_netbox_dhcp.py
@@ -7,7 +7,7 @@ from utilities.testing.api import APITestCase
 
 class NetBoxDHCPVersionTestCase(SimpleTestCase):
     def test_version(self):
-        assert __version__ == "0.1.2"
+        assert __version__ == "0.1.3"
 
 
 class AppTest(APITestCase):
